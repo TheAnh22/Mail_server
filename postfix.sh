ac984fa8ed3ff6cac3dd2a9eeac10f63d62e5c3f
root <root@ubuntu.sgu.edu.vn>
2024-11-04 10:12:51 +0700

    Day code lan 2

--- postfix.sh
+++ postfix.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
-clear
-echo "hello"
+
+echo "====================="
+echo "Chọn một tùy chọn:"
+echo "1. Thay doi domain"
+echo "2. Config Dovecot"
+echo "3. Manage User"
+echo "4. Exit"
+echo "====================="
+read -p "Nhập lựa chọn của bạn (1-4): " choice
+case $choice in
+        1)      
+            clear
+            read -p "Nhập tên miền mới cho Postfix: " NEW_DOMAIN
+            if [ -z "$NEW_DOMAIN" ]; then
+            	echo "Ten mien khong duoc de trong!"
+            	exit 1
+            fi
+            sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.origin
+            sudo sed -i "s/^mydomain = .*/mydomain = $NEW_DOMAIN/" /etc/postfix/main.cf
+            service postfix restart
+            ;;
+        2)
+            clear 
+            bash /home/theanh/Mail_server/dovecot.sh
+            ;;
+        3)
+            clear
+            bash /home/theanh/Mail_server/user.sh
+            ;;
+        4)
+            echo "Thoát chương trình."
+            clear
+            exit 0
+            ;;
+        *)
+            clear
+            echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
+            ;;
+esac
--- test.sh
+++ test.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+	postfixStatus=$(systemctl is-active postfix)
+	dovecotStatus=$(systemctl is-active dovecot)
+	if [ "$dovecotStatus" == "active" ] && [ "$postfixStatus" == "active" ]; then
+	    echo "Postfix and Dovecot dang chay"
+	else
+	    read -p "Postfix va Dovecot chua duoc cai dat,bat dau cai dat (y/n):" answer
+	    if [ "$answer" == "y" ]; then
+	    	bash /home/theanh/Mail_server/install.sh
+	    else 
+	    	clear
+	    	echo "Thoat ung dung"
+	    	
+	    fi
+	fi
+	
 while true; do
     # Hiển thị menu
+	
     echo "====================="
     echo "Chọn một tùy chọn:"
     echo "1. Config Postfix"
@@ -12,17 +28,17 @@ while true; do
     read -p "Nhập lựa chọn của bạn (1-4): " choice
 
     case $choice in
-        1)
-	    clear        
-            bash /home/theanh/postfix.sh
+        1)      
+            clear
+            bash /home/theanh/Mail_server/postfix.sh
             ;;
         2)
-            echo "Thư mục hiện tại là:"
-            pwd
+            clear 
+            bash /home/theanh/Mail_server/dovecot.sh
             ;;
         3)
-            echo "Các tập tin trong thư mục hiện tại:"
-            ls
+            clear
+            bash /home/theanh/Mail_server/user.sh
             ;;
         4)
             echo "Thoát chương trình."
@@ -30,12 +46,11 @@ while true; do
             exit 0
             ;;
         *)
+            clear
             echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
             ;;
     esac
 
     # Dừng lại để người dùng nhấn Enter trước khi quay lại menu
-    read -p "Nhấn Enter để tiếp tục..."
-    clear
 done
 
