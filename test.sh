1da1dd8a721e3403137d43186653b10fbca7ffc7
root <root@ubuntu.sgu.edu.vn>
2024-11-11 07:41:37 +0700

    Final_Update

--- test.sh
+++ test.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+clear
+PATHWAY=$(pwd)
 
-	postfixStatus=$(systemctl is-active postfix)
-	dovecotStatus=$(systemctl is-active dovecot)
-	if [ "$dovecotStatus" == "active" ] && [ "$postfixStatus" == "active" ]; then
-	    echo "Postfix and Dovecot dang chay"
-	else
-	    read -p "Postfix va Dovecot chua duoc cai dat,bat dau cai dat (y/n):" answer
-	    if [ "$answer" == "y" ]; then
+postfixStatus=$(systemctl is-active postfix)
+dovecotStatus=$(systemctl is-active dovecot)
+if [ "$dovecotStatus" == "active" ] && [ "$postfixStatus" == "active" ]; then
+	echo "Postfix and Dovecot dang chay"
+else
+	read -p "Postfix va Dovecot chua duoc cai dat,bat dau cai dat (y/n):" answer
+	if [ "$answer" == "y" ]; then
 	    	bash /home/theanh/Mail_server/install.sh
-	    else 
-	    	clear
+	else 
+		clear
 	    	echo "Thoat ung dung"
+	    	exit 0
 	    	
-	    fi
 	fi
+fi
 	
 while true; do
     # Hiển thị menu
@@ -21,29 +24,47 @@ while true; do
     echo "====================="
     echo "Chọn một tùy chọn:"
     echo "1. Sua Domain"
-    echo "2. Sua dia chi IP"
-    echo "3. Them User"
-    echo "4. Xoa User"
-    echo "5. Doi mat khau user"
-    echo "6. Xuat toan bo user"
-    echo "7. Exit"
+    echo "2. Sua Nameserver"
+    echo "3. Sua dia chi IP"
+    echo "4. Them User"
+    echo "5. Xoa User"
+    echo "6. Doi mat khau user"
+    echo "7. Xuat toan bo user"
+    echo "8. Exit"
     echo "====================="
     read -p "Nhập lựa chọn của bạn (1-4): " choice
 
     case $choice in
-        1)      
+    	#Nhập tên miền mới
+    	
+	 1)      
             clear
             read -p "Nhap ten mien moi " NEW_DOMAIN
             if [ -z "$NEW_DOMAIN" ]; then
             	echo "Ten mien khong duoc de trong!"
             	exit 1
+            else
+            	sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.origin
+            	sudo sed -i "s/^mydomain = .*/mydomain = $NEW_DOMAIN/" /etc/postfix/main.cf 
+            	service postfix restart
+            	service dovecot restart
             fi
-            sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.origin
-            sudo sed -i "s/^mydomain = .*/mydomain = $NEW_DOMAIN/" /etc/postfix/main.cf
-            sudo sed -i "s/^myhostname =.*/myhostname = mail.$NEW_DOMAIN/" /etc/postfix/main.cf 
-            service postfix restart
+            
             ;;
-        2)
+         2)
+            clear
+            read -p "Nhap ten mien mail server" NEW_HOSTNAME
+            if [ -z "$NEW_HOSTNAME" ]; then
+            	echo "Ten hostname khong duoc de trong"
+            else
+            	sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.origin
+            	sudo sed -i "s/^myhostname = .*/myhostname = mail.$NEW_HOSTNAME/" /etc/postfix/main.cf 
+            	service postfix restart
+            	service dovecot restart
+            fi
+            ;;
+       #Nhập dãy địa chỉ mới 
+         3)
             clear 
             read -p "Nhap day dia chi " NEW_IP
             if [ -z "$NEW_IP" ]; then
@@ -54,7 +75,8 @@ while true; do
             sudo sed -i "s/^mynetworks = .*/mynetworks = $NEW_IP/" /etc/postfix/main.cf 
             service postfix restart
             ;;
-        3)
+       #Thêm user
+	 4)
             clear
             read -p "Nhap ten user:" USERNAME
             read -p "Nhap password:" PASSWORD
@@ -62,25 +84,69 @@ while true; do
             echo "$USERNAME:$PASSWORD" | chpasswd
        	    clear
             ;;
-        4)
+       #Xóa user 
+	 5)
             clear
-            read -p "Nhap ten nguoi dung:" DELUSER
+            awk -F ":" '$3 >= 1000 { print $1 }' /etc/passwd
+            read -p "Nhap ten nguoi dung(bo trong de thoat):" DELUSER
+            if [ -z "$DELUSER" ]; then
+            	echo ""
+            	clear
+            else
+            	if  id "$DELUSER" &>/dev/null ; then
+  			sudo deluser --remove-all-files $DELUSER
+  			clear
+  			echo "Da xoa user!"
+		else
+  			echo "User '$DELUSER' không tồn tại trong hệ thống."
+		fi
+            fi
             ;;
-        5) 
+        #Doi password
+        6) 
             clear
-            awk -F: '$3 >= 1000 { print $1 }' /etc/passwd
+            awk -F ":" '$3 >= 1000 { print $1 }' /etc/passwd
+            read -p "Nhap ten user can doi password:" CHANGEUSER
+            case "$CHANGEUSER" in
+            	"")
+            	   clear
+            	   ;;
+            	*)
+            	   if  id "$CHANGEUSER" &>/dev/null ; then
+  			read -p "Nhap password moi:" NEWPASS
+            		read -p "Nhap lai password:" AGAINPASS
+            			if [ "$NEWPASS" = "$AGAINPASS" ]; then
+            				echo "$CHANGEUSER:$NEWPASS" | chpasswd
+            				if [ $? -eq 0 ]; then
+            					
+            					read -p "Doi thanh cong, nhan ENTER de tiep tuc"
+            				else
+            					read -p "Doi that bai, nhan ENTER de tiep tuc" ENTER
+            				fi
+            			else 
+            				echo "Pasword khong khop!"
+            				exit 1
+            			fi
+	    	   else
+  		       echo "User '$DELUSER' không tồn tại trong hệ thống."
+	           fi
+	           ;;
+	      esac
             ;;
-        6)
-            echo "Thoát chương trình."
-            clear
-            exit 0
+        7)
+	    clear 
+            awk -F ":" '$3 >= 1000 { print $1 }' /etc/passwd
+            ;;
+        8)
+      	    clear
+	    exit 0
+	    
             ;;
         *)
             clear
             echo "Lựa chọn không hợp lệ. Vui lòng chọn lại."
             ;;
     esac
-
-    # Dừng lại để người dùng nhấn Enter trước khi quay lại menu
+    clear
 done
 
